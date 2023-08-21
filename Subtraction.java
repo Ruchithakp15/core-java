900eda98a7c80717cd2a8e60ec55efda199342e1
Ruchithakp <ruchitharuchi74.kp@gmail.com>
2023-08-21 11:00:09 +0530

    Commiting array related example thru Subtraction

---
+++ Subtraction.java
@@ -0,0 +1,34 @@
+class Subtraction {
+public static void main(String sub[]){
+System.out.println("Main Started");
+sub(8,6);
+sub(23,12);
+sub(1,1);
+sub(56,46);
+sub(12,11);
+sub(67,32);
+sub(100,99);
+sub(56,8);
+sub(66,21);
+sub(96,36);
+sub(3,2);
+sub(49,21);
+sub(94,23);
+sub(56,46);
+sub(22,11);
+sub(11,9);
+sub(41,40);
+sub(23,19);
+sub(77,32);
+sub(50,44);
+System.out.println("Main ended");
+}
+public static void sub(int a,int b){
+System.out.println("Method Started");
+System.out.println("Subtraction of Two Numbers Is : ");
+System.out.println(a-b);
+System.out.println("Method ended");
+}
+}
+
+
