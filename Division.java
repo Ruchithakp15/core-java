4c521eca4f4fd35418efbcfdaa6a43ff41f2d48e
Ruchithakp <ruchitharuchi74.kp@gmail.com>
2023-08-21 11:37:01 +0530

    commiting method thru Divison

---
+++ Division.java
@@ -0,0 +1,34 @@
+class Division {
+public static void main(String div[]){
+System.out.println("Main Started");
+div(6,3);
+div(7,9);
+div(63,21);
+div(13,36);
+div(13,30);
+div(26,36);
+div(5,10);
+div(26,20);
+div(60,70);
+div(100,60);
+div(19,12);
+div(22,33);
+div(4,2);
+div(3,9);
+div(1,1);
+div(36,30);
+div(96,12);
+div(6,60);
+div(31,14);
+div(16,23);
+System.out.println("Main ended");
+}
+public static void div(int a,int b){
+System.out.println("Method Started");
+System.out.println("Division of Two Numbers Is : ");
+System.out.println(a/b);
+System.out.println("Method ended");
+}
+}
+
+
